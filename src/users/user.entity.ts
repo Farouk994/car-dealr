2717100e687f66e64c949120779f6de0490c530c
Farouk <faroukkisuule55@gmail.com>
2022-03-12 17:23:21 -0500

    Added routes for finding user by id and also querying user by email

--- src/users/user.entity.ts
+++ src/users/user.entity.ts
@@ -1,13 +1,36 @@
-import {Entity, Column, PrimaryGeneratedColumn} from "typeorm";
+import {
+  Entity,
+  Column,
+  PrimaryGeneratedColumn,
+  AfterInsert,
+  AfterRemove,
+  AfterUpdate,
+} from 'typeorm';
 
 @Entity()
 export class User {
-    @PrimaryGeneratedColumn()
-    id : number;
+  @PrimaryGeneratedColumn()
+  id: number;
 
-    @Column()
-    email : string;
-    
-    @Column()
-    password: string;
+  @Column()
+  email: string;
+
+  @Column()
+  password: string;
+
+  //? @Hooks Decorators in nestJs
+  // Decorator applied to a method defined inside the entity
+  // Whenever User is inserted this method will always run and log in console
+  @AfterInsert()
+  logInsert() {
+    console.log('Inserted User', this.id);
+  }
+  @AfterUpdate()
+  logUpdate() {
+    console.log('Updated User', this.id);
+  }
+  @AfterRemove()
+  logRemove() {
+    console.log('Removed User', this.id);
+  }
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -7,7 +7,36 @@ import { User } from './user.entity';
 export class UsersService {
   constructor(@InjectRepository(User) private repo: Repository<User>) {}
   create(email: string, password: string) {
+    // create() creates new instance of the user entity
+    // Then we will save it to db after verification
     const user = this.repo.create({ email, password });
+
+    // save() Used for actual persistance to db
+    // If we just pass in the object for email and password then hooks wont be
+    // executed, meaning we wont know if there are bugs or anything
     return this.repo.save(user);
+  }
+
+  findOne(id: number) {
+    return this.repo.findOne(id);
+  }
+
+  find(email: string) {
+    return this.repo.find({ email });
+  }
+
+  async update(id: number, attrs: Partial<User>) {
+    const user = await this.findOne(id);
+    if (!user) throw new Error('user not found!');
+    // This method will get the value we have in attrs that was updated from the user
+    // entity and assign it to the user
+    Object.assign(user, attrs);
+    return this.repo.save(user);
+  }
+
+  async remove(id: number) {
+    const user = await this.findOne(id);
+    if (!user) throw new Error('user not found');
+    return this.repo.remove(user);
   }
 }
